2176114de3f36c54a45f226807cd1e6258e6419e
Nonprawich I <40120130+npwitk@users.noreply.github.com>
2024-12-30 11:03:43 +0700

    Initial Commit

---
+++ jason-in-action/jason_in_actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  jason_in_actionApp.swift
+//  jason-in-action
+//
+//  Created by Nonprawich I. on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct jason_in_actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
